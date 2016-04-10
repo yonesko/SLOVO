162110b01fed38496e90521f31faf0a912400a31
Gleb Danichev <yonesko@gmail.com>
2016-04-10 15:13:21 +0300

    refactoring

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -18,45 +18,48 @@ import java.util.concurrent.TimeUnit;
  */
 public class Main {
     private static final long DELAY_H = 5;
+    private static final long PORTION_NUM = 5;
     private static final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
     private static final Queue<String> wantedWords = new LinkedList<>(Arrays.asList("аа", "мафия"));
-    private static long lastDateMils = 0;
     private static List<FreqEntity> candidates;
 
     public static void main(String[] args) throws ParseException, IOException, URISyntaxException, java.text.ParseException {
         List<WallPost> published = new ArrayList<>();
-        WordInfo wordInfo;
+        WordInfo nextPost;
+        long nextPostDateMils, lastDateMils;
 
         candidates = FetchFreq.getFreqDict();
         published = VK.getPosts("owner");
         lastDateMils = sdf.parse("10/04/2016 22:55:00").getTime();
         System.out.println("lastDateMils is " + sdf.format(new Date(lastDateMils)));
 
-        //remove published
+        //remove already published from candidates
         for (WallPost s : published)
             if (candidates.contains(FetchFreq.get(s.getWord())))
                 candidates.remove(FetchFreq.get(s.getWord()));
 
         //publish portion
-        for (int i = 0; i < 6 && candidates.size() > 0; i++) {
+        for (int i = 0; i < PORTION_NUM && candidates.size() > 0; i++) {
             System.out.println("---------" + i + "----------");
-            wordInfo = nextWord();
+            nextPost = nextWord();
 
-            if (wordInfo != null && wordInfo.isPublishable()) {
-                System.out.println(String.format(
-                        "Trying to publish %s to date %s",
-                        wordInfo.getName(),
-                        sdf.format(new Date(lastDateMils + TimeUnit.HOURS.toMillis(DELAY_H)))));
-                if (VK.wallPost(wordInfo.toPublish(), TimeUnit.MILLISECONDS.toSeconds(lastDateMils) + TimeUnit.HOURS.toSeconds(DELAY_H))) {
+            if (nextPost != null)
+                if (nextPost.isPublishable()) {
+                    nextPostDateMils = lastDateMils + TimeUnit.HOURS.toMillis(DELAY_H);
+                    System.out.println(String.format(
+                            "Trying to publish %s to date %s",
+                            nextPost.getName(),
+                            sdf.format(new Date(nextPostDateMils))));
+                    if (VK.wallPost(nextPost.toPublish(), TimeUnit.MILLISECONDS.toSeconds(nextPostDateMils))) {
 //                    candidates.remove(fe);
-                    System.out.println("OK");
-                    lastDateMils += TimeUnit.HOURS.toMillis(DELAY_H);
+                        System.out.println("OK");
+                        lastDateMils += TimeUnit.HOURS.toMillis(DELAY_H);
+                    } else
+                        System.out.println("NOT OK");
+                } else {
+                    i--;
+                    System.out.println(nextPost.getName() + " is not publishable");
                 }
-            }
-            else {
-                i--;
-                System.out.println("is not publishable");
-            }
             System.out.println("-------------------");
         }
     }
